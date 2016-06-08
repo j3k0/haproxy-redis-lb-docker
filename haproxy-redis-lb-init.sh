9114835abb7e69b3ed3b620859f4df7dec41d55f
gprivitera <gprivitera@streamatica.com>
2016-06-08 16:08:42 -0400

    Initial commit

---
+++ haproxy-redis-lb-init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+SERVERS_IPS=$1
+
+IPS=(${SERVERS_IPS//;/ })
+N=0
+
+for i in "${IPS[@]}"; do
+  N++
+  echo "server server$N $i:6379 check inter 1s" >> /usr/local/etc/haproxy/haproxy.cfg
+done
+
+haproxy -f "/usr/local/etc/haproxy/haproxy.cfg"
