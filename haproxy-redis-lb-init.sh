8ff38d54cc78a4f79a6c9e315c011cbc92a16437
Jean-Christophe Hoelt <hoelt@fovea.cc>
2016-10-27 20:43:28 +0300

    Allow an optional port definition to each server
    format: "host[:port];host[:port]..."

--- haproxy-redis-lb-init.sh
+++ haproxy-redis-lb-init.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
+set -e
 
-SERVERS_IPS=$1
+function serverDefinitions() {
+  SERVERS=$1
+  SERVERS=(${SERVERS//;/ })
+  N=0
+  for i in "${SERVERS[@]}"; do
+    SERVER=(${i//:/ })
+    SERVER_HOST=${SERVER[0]}
+    SERVER_PORT=${SERVER[1]}
+    if [ -z $SERVER_PORT ]; then SERVER_PORT=6379; fi
+    ((N++))
+    echo "server server$N $SERVER_HOST:$SERVER_PORT check inter 1s"
+  done
+}
 
-IPS=(${SERVERS_IPS//;/ })
-N=0
+# CLI option `--dry-run` allows to test the serverDefinitions() method.
+if [ "_$1" = "_--dry-run" ]; then
+  serverDefinitions "$2"
+  exit 0
+fi
 
-for i in "${IPS[@]}"; do
-  ((N++))
-  echo "server server$N $i:6379 check inter 1s" >> /usr/local/etc/haproxy/haproxy.cfg
-done
-
+serverDefinitions "$1" >> /usr/local/etc/haproxy/haproxy.cfg
 haproxy -f "/usr/local/etc/haproxy/haproxy.cfg"
